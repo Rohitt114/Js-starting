9ebc61ed5214c5579ada1b11c49c9293c4747664
Rohitt114 <128043437+Rohitt114@users.noreply.github.com>
2026-01-17 11:56:37 +0000

    datatypes

---
+++ basics_01/datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // Means treat all JS code as the newer version
+
+//alert(3+3) //we are using node.js, not browser
+
+console.log(6+9), console.log("Its not 69") // This work but poor for readability
+
+
+let name = "Rohit" // string datatype
+let age = 18 // integer
+let isPresent = false //boolean
+
+/*
+interger => 2 to the power 53
+bigint
+string ""
+boolean true/false
+null => it is a value, an empty value but it's not defined
+undefined
+symbol => unique
+
+object
+*/
+
+console.log(typeof "rohit");
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
+
